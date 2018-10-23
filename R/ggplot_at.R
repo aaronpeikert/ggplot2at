384dd971a78d0ee2baaf2869cfee82fc05040466
Maximilian Stefan Ernst <34346372+Maximilian-Stefan-Ernst@users.noreply.github.com>
2018-10-23 18:50:01 +0200

    Fixes#2 also take xvars only

--- R/ggplot_at.R
+++ R/ggplot_at.R
@@ -15,15 +15,26 @@
 #' @importFrom rlang !!!
 #' @export
 
-ggplot_at <- function(data, xvars, yvars, limit = 100){
-  xvars <- tidyselect::vars_select(names(data), !!!xvars)
-  yvars <- tidyselect::vars_select(names(data), !!!yvars)
-  to_plot <- expand.grid(x = xvars, y = yvars, stringsAsFactors = FALSE)
-  if(nrow(to_plot)>limit)stop("You're about to plot more then ",
-                              limit,
-                              " plots. Set your limit at least to ",
-                              nrow(to_plot), ".")
-  out <- purrr::map2(to_plot$x, to_plot$y, ~ggplot(data, aes(x = !!sym(.x), y = !!sym(.y))))
-  names(out) <- paste(to_plot$x, to_plot$y, sep = "-")
+ggplot_at <- function(data, xvars, yvars = NULL, limit = 100){
+  if (is.null(yvars)){
+    xvars <- tidyselect::vars_select(names(data), !!!xvars)
+    to_plot <- data.frame(x = xvars, stringsAsFactors = FALSE)
+    if(nrow(to_plot)>limit)stop("You're about to plot more then ",
+                                limit,
+                                " plots. Set your limit at least to ",
+                                nrow(to_plot), ".")
+    out <- purrr::map(to_plot$x, ~ggplot(data, aes(x = !!sym(.x))))
+    names(out) <- to_plot$x
+  }else{
+    xvars <- tidyselect::vars_select(names(data), !!!xvars)
+    yvars <- tidyselect::vars_select(names(data), !!!yvars)
+    to_plot <- expand.grid(x = xvars, y = yvars, stringsAsFactors = FALSE)
+    if(nrow(to_plot)>limit)stop("You're about to plot more then ",
+                                limit,
+                                " plots. Set your limit at least to ",
+                                nrow(to_plot), ".")
+    out <- purrr::map2(to_plot$x, to_plot$y, ~ggplot(data, aes(x = !!sym(.x), y = !!sym(.y))))
+    names(out) <- paste(to_plot$x, to_plot$y, sep = "-")
+  }
   return(out)
 }
